e97a7b6f61c44ce5aa2383d8f97bf59c1f35127b
aralvi <aralvi143@gmail.com>
2023-09-27 17:32:27 +0800

    withdraw day 5 added again

--- public/admin/js/custom.js
+++ public/admin/js/custom.js
@@ -1,15 +1,15 @@
-// $(document).ready(function () {
-//     var currentDate = new Date();
-//     var fifthOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 6);
+$(document).ready(function () {
+    var currentDate = new Date();
+    var fifthOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 6);
 
-//     if (currentDate > fifthOfMonth) {
-//         // Disable form submission
-//         $("#transaction_form").submit(function (e) {
-//             e.preventDefault();
-//             alert("You can only request between 1st to 5th date of every month");
-//         });
+    if (currentDate > fifthOfMonth) {
+        // Disable form submission
+        $("#transaction_form").submit(function (e) {
+            e.preventDefault();
+            alert("You can only request between 1st to 5th date of every month");
+        });
 
-//         // Optionally, you can disable the submit button to provide visual feedback
-//         $("#transaction_form input[type='submit']").prop("disabled", true);
-//     }
-// });
+        // Optionally, you can disable the submit button to provide visual feedback
+        $("#transaction_form input[type='submit']").prop("disabled", true);
+    }
+});
